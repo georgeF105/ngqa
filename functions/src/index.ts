a4aabe2576c114018713ec42a313dd66374c4aa2
george francis <georgefrancis105@gmail.com>
2018-08-26 16:41:46 +1200

    Make graphql functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
 
+import setupGraphQLServer from './graphql';
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send('Hello from Firebase!');
+});
+
+
+const graphQLServer = setupGraphQLServer();
+
+// https://us-central1-<project-name>.cloudfunctions.net/api
+export const api = functions.https.onRequest(graphQLServer);
