3842be794c04f1ed8e8c1dae8870eef08efd95a6
george francis <georgefrancis105@gmail.com>
2018-09-08 15:22:30 +1200

    Use gql in question

--- src/app/questions/question-details/question-details.component.ts
+++ src/app/questions/question-details/question-details.component.ts
@@ -1,7 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FirebaseService } from '../../database/firebase/firebase.service';
-import { Store, Question } from '../../database';
-import { Observable, Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Question } from '@ngqa/models';
+import { Observable } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map, switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+
+const QUESTION_QUERY = gql`
+  query CurrentQuestion($id: String!) {
+  question (id: $id) {
+    key
+    body
+    answers {
+      key
+      body
+      user {
+        name
+      }
+      votes
+    }
+    user {
+      name
+    }
+  }
+}
+`;
 
 @Component({
   selector: 'app-question-details',
@@ -12,10 +35,28 @@ export class QuestionDetailsComponent implements OnInit {
   public question$: Observable<Question>;
 
   constructor(
-    private _firebaseService: FirebaseService<Store>
+    private _apollo: Apollo,
+    private _activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.question$ = this._firebaseService.getItem(['questions', 'dummyKey']);
+    this.question$ = this.getQuestion();
+  }
+
+  private getQuestion (): Observable<Question> {
+    const questionKey$ = this._activatedRoute.params.pipe(
+      map(params => params.key)
+    );
+    return questionKey$.pipe(
+      switchMap(key => {
+        return this._apollo.query<{ question: Question }>({
+          query: QUESTION_QUERY,
+          variables: {
+            id: key
+          }
+        });
+      }),
+      map(result => result.data.question)
+    );
   }
 }
